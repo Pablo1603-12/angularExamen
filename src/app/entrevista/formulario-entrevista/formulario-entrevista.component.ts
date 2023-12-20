2ac2f1a93d432c799940e2f0ad489e91d00b917e
DESKTOP-SF72SGS\Puesto19 <pablomarquinezjimenez3@gmail.com>
2023-12-20 12:15:57 +0100

    tercer commit

--- src/app/entrevista/formulario-entrevista/formulario-entrevista.component.ts
+++ src/app/entrevista/formulario-entrevista/formulario-entrevista.component.ts
@@ -3,6 +3,9 @@ import { entrevista } from 'src/app/modelos/entrevista.model';
 import { ActivatedRoute } from '@angular/router';
 
 import { EntrevistasService } from 'src/app/servicios/entrevistas/entrevistas.service';
+import { CandidatoService } from 'src/app/servicios/candidato/candidato.service';
+import { PuestoTrabajoService } from 'src/app/servicios/puestoTrabajo/puesto-trabajo.service';
+
 
 @Component({
   selector: 'app-formulario-entrevista',
@@ -17,17 +20,27 @@ export class FormularioEntrevistaComponent {
     
     realizada: undefined
   };
-candidatos: any;
-puesto: any;
 
-  constructor(private route: ActivatedRoute, private entrevistaService: EntrevistasService) { }
+  candidatos: any[] = [];
+  puestosTrabajo: any[] = [];
+
+
+  constructor(
+    private route: ActivatedRoute,
+    private entrevistaService: EntrevistasService,
+    private candidatoService: CandidatoService,
+    private puestoTrabajoService: PuestoTrabajoService
+  ) {}
 
   ngOnInit(): void {
-    const entrevista = this.route.snapshot.paramMap.get('id');
-    if (entrevista) {
-      this.cargarEntrevista(entrevista);
+    const entrevistaId = this.route.snapshot.paramMap.get('id');
+    if (entrevistaId) {
+      this.cargarEntrevista(entrevistaId);
+      this.obtenerCandidatos(); // Asegúrate de implementar este método
+      this.obtenerPuestosTrabajo(); // Asegúrate de implementar este método
     }
   }
+  
 
   cargarEntrevista(entrevista: string): void {
     this.entrevistaService.getEntrevistas(entrevista).subscribe(
@@ -39,16 +52,37 @@ puesto: any;
       }
     );
   }
+  obtenerCandidatos(): void {
+    this.candidatoService.getCandidatos().subscribe(
+      (candidatos) => {
+        this.candidatos = candidatos;
+      },
+      (error) => {
+        console.error('Error al obtener candidatos:', error);
+      }
+    );
+  }
+
+  obtenerPuestosTrabajo(): void {
+    this.puestoTrabajoService.getPuestoTrabajo().subscribe(
+      (puestosTrabajo) => {
+        this.puestosTrabajo = puestosTrabajo;
+      },
+      (error) => {
+        console.error('Error al obtener puestos de trabajo:', error);
+      }
+    );
+  }
 
   guardarCambios(): void {
     if (this.entrevista) {
            // Agregar una entrevista
       this.entrevistaService.agregarEntrevista(this.entrevista).then(
         () => {
-          console.log('Candidato agregado con éxito.');
+          console.log('entrevista agregada con éxito.');
         },
         (error) => {
-          console.error('Error al agregar nuevo candidato:', error);
+          console.error('Error al agregar nueva entrevista', error);
         }
       );
     }
